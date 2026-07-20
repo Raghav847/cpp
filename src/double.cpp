a7d97b6febb560ba29ac4d86544788071f67caaa
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-07-19 17:47:27 -0700

    double

---
+++ src/double.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter a integer: ";
+    int num{};
+    std::cin >> num;
+
+    std::cout << "Double of the int: " << num * 2 << '\n';
+
+    return 0;
+
+}
